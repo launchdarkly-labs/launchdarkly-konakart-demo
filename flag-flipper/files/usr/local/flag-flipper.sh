033e23ba67a13454f2afda4b3408f77a97a46c0d
Michael Vittiglio <michael.vittiglio@gmail.com>
2021-04-15 15:55:38 -0400

    Flag flipper now supports environment name changes via env file.
    The flipper will also not automatically turn on the flag on the outset but after the initial SLEEP_DURATION has expired.

--- flag-flipper/files/usr/local/flag-flipper.sh
+++ flag-flipper/files/usr/local/flag-flipper.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 while :
 do
-    curl -i -X PATCH -H "Authorization: $API_KEY" -H "Content-Type: application/json" https://app.launchdarkly.com/api/v2/flags/$PROJECT_ID/$FLAG_ID -d '{"comment": "Flag turned on via automation using REST API call.", "patch": [{"op":"replace", "path":"/environments/production/on", "value": true}] }'
-    sleep 3600
+    sleep SLEEP_DURATION_IN_SECONDS
+    printf '{"comment": "Flag turned on via automation using REST API call.", "patch": [{"op":"replace", "path":"/environments/' > data
+    printf "%s" $ENVIRONMENT_NAME >> data
+    printf '/on", "value": true}] }' >> data
+    curl -i -X PATCH -H "Authorization: $API_KEY" -H "Content-Type: application/json" https://app.launchdarkly.com/api/v2/flags/$PROJECT_NAME/$FLAG_ID -d @data
 done
