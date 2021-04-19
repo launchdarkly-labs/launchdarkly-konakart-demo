cda714e112cf6afc2acd196b42325ac3a29b2b2b
Michael Vittiglio <michael.vittiglio@gmail.com>
2021-04-19 10:29:40 -0400

    Flag flipper now supports a time range for delay. Previously this was a single value which some tools would begine to ignore (looking at you dynatrace).

--- flag-flipper/files/usr/local/flag-flipper.sh
+++ flag-flipper/files/usr/local/flag-flipper.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 while :
 do
-    sleep SLEEP_DURATION_IN_SECONDS
+    let SLEEP_DELAY_RANGE_SIZE=$SLEEP_DELAY_CEILING-$SLEEP_DELAY_FLOOR+1 # Adding 1 to ensure we do not mod 0 on the next line.
+    let SLEEP_DELAY_DURATION=$RANDOM%$SLEEP_DELAY_RANGE_SIZE
+    let SLEEP_DELAY_ACTUAL=$SLEEP_DELAY_DURATION+$SLEEP_DELAY_FLOOR
+    echo "Sleeping for $SLEEP_DELAY_ACTUAL seconds (range $SLEEP_DELAY_FLOOR - $SLEEP_DELAY_CEILING seconds) until activating flag..."
+    sleep $SLEEP_DELAY_ACTUAL
     printf '{"comment": "Flag turned on via automation using REST API call.", "patch": [{"op":"replace", "path":"/environments/' > data
     printf "%s" $ENVIRONMENT_NAME >> data
     printf '/on", "value": true}] }' >> data
